f071434d0039026ec6f218a4aaabe5666531fc27
Ebram Sobhy <116510911+Ebramsobhy@users.noreply.github.com>
2023-12-28 05:01:41 +0200

    Delete demo directory

--- demo/__manifest__.py
+++
@@ -1,5 +0,0 @@
-{
-    'name': "ITI43",
-    'author': "iti",
-    'data': ['views/iti_student.views.xml']
-}
--- demo/models/iti_student.py
+++
@@ -1,9 +0,0 @@
-from odoo import models , fields
-
-class ITIStudent(models.Model):
-    _name = 'iti.student'
-    
-    name = fields.Char()
-    age = fields.Integer()
-    gender = fields.Selection([('Male', 'male') , ('Female' , 'female')])
-    accepted = fields.Boolean()
